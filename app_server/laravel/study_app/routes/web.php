346eeff8aaa564f05bb1c003212ec1037dc35292
RyosukeSomeya <rx.78.0618@gmail.com>
2020-01-26 02:39:36 +0900

    JSONを返すrest機能の実装

--- app_server/laravel/study_app/routes/web.php
+++ app_server/laravel/study_app/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('rest', 'RestappController');
+
 // Route::get('hello', function () {
 //     return view('hello.index');
 // });
